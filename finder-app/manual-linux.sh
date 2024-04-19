976d85d9b38a974ceb782a9027e4684694f5b347
anhngduc <anh_nguyen.1991@outlook.com>
2024-04-19 04:07:20 -0700

    assigment 2 fix

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -63,7 +63,7 @@ cd rootfs
 mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
 mkdir -p usr/bin usr/lib usr/sbin
 mkdir -p var/log
-tree
+# tree
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
