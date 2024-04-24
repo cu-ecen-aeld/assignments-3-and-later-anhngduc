109b69ad62f653ed1f396c41df5dd745faa62d8d
anhngduc <anh_nguyen.1991@outlook.com>
2024-04-23 23:47:13 -0700

    assignment 4 part 2"

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,11 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+# CONF_PATH=./conf
+# LOCAL=./
+CONF_PATH=/etc/finder-app/conf
+LOCAL=
+username=$(cat "${CONF_PATH}/username.txt")
 
 if [ $# -lt 3 ]
 then
@@ -32,7 +36,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat "${CONF_PATH}/assignment.txt"`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -55,10 +59,10 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	"$LOCAL"writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$("$LOCAL"finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
